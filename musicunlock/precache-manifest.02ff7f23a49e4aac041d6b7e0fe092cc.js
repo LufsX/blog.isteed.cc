3703f7b927da636b8cf1d331f7a742a9d90ac4b1
Lufsx <lufs00@outlook.com>
2020-01-20 21:18:48 +0800

    Site updated: 2020-01-20 21:18:46
    Former-commit-id: 737d5d0633bd467898682ae130c13a0b074f5ee6 [formerly 8a62a31d087a1c16e92faa60c6998508302017ba]
    Former-commit-id: 392b50443c1abe69476a940b1784a6e81c64c7fe

---
+++ musicunlock/precache-manifest.02ff7f23a49e4aac041d6b7e0fe092cc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "569f8b7ef691077c6977",
+    "url": "css/app.a6b81b55.css"
+  },
+  {
+    "revision": "2a7cdeaa42d85cf985b6",
+    "url": "css/chunk-vendors.d39e9fad.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "d078d0cbdcadc445d593b6ddb0b6ef78",
+    "url": "index.html"
+  },
+  {
+    "revision": "569f8b7ef691077c6977",
+    "url": "js/app.54f956b4.js"
+  },
+  {
+    "revision": "2a7cdeaa42d85cf985b6",
+    "url": "js/chunk-vendors.b078246e.js"
+  },
+  {
+    "revision": "28f3e13ec88073aa1b85b7d66358f613",
+    "url": "manifest.json"
+  }
+]);

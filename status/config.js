28ef91354a75fd550c88e714a3ca0d1b52c2644b
LufsX <LufsX@users.noreply.github.com>
2024-10-15 20:36:43 +0800

    GitHub Actions Auto Builder

---
+++ status/config.js
@@ -0,0 +1,29 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: '服务状态',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'ur980256-f705d0f0ea025d420e84f125'
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: false,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'Homepage',
+      url: '/'
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/yb/uptime-status'
+    }
+  ],
+};
